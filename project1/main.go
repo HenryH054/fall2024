ff93565dd58c1be815c396369a163b52105d2033
HenryH054 <henryphutchinson@gmail.com>
2024-08-26 20:43:38 -0600

    the start of karatsuba

--- project1/main.go
+++ project1/main.go
@@ -1,54 +1,46 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"math"
 	"math/big"
 )
 
-func parseNum(num *big.Int, bits int) (int32, *big.Int) {
-	// returns the top bits (must have bit size set to < 32) and the rest of the big int
-	var sigValues int32
-	var remainder big.Int
-	length := num.BitLen()
-	if num.BitLen() <= bits {
-		return int32(num.Int64()), nil
+func karatsuba(x *big.Int, y *big.Int) *big.Int {
+	// base case multiplies numbers together once they are small enough
+	if x.Cmp(big.NewInt(2)) == -1 {
+		return x.Mul(x, y)
 	}
-	lowerBitFence := length - bits
-	lowerBits := big.NewInt(1).Lsh(big.NewInt(1), uint(lowerBitFence))
-	lowerBits = lowerBits.Sub(lowerBits, big.NewInt(1))
 
-	remainder = *remainder.And(num, lowerBits)
-	sigValues = int32(big.NewInt(1).Rsh(num, uint(lowerBitFence)).Int64())
-	return sigValues, &remainder
-}
+	// gets the middle of the numbers by binary length
+	m := max(x.BitLen(), y.BitLen())
+	m2 := m / 2
 
-func karatsuba(x *big.Int, y *big.Int) (*big.Int, error) {
-	base := 2.0
-	m := 31.0
-	z := math.Pow(base, m)
-	a, b := parseNum(x, int(m))
-	c, d := parseNum(y, int(m))
-	if b == nil && d == nil {
-		return big.NewInt(int64(a) * int64(c)), nil
-	}
-	if b == nil || d == nil {
-		return big.NewInt(0), errors.New("Values of unequal length")
-	}
-	xy := big.NewInt(1)
-	return xy, nil
+	mask := new(big.Int).Lsh(big.NewInt(1), uint(m2))
+	mask.Sub(mask, big.NewInt(1))
+
+	lox := new(big.Int).And(x, mask)
+	upx := new(big.Int).Rsh(x, uint(m2))
+
+	loy := new(big.Int).And(y, mask)
+	upy := new(big.Int).Rsh(y, uint(m2))
+
+	z0 := karatsuba(lox, loy)
+	z1 := karatsuba(new(big.Int).Add(lox, upx), new(big.Int).Add(loy, upy))
+	z2 := karatsuba(upx, upy)
+
+	unit1 := new(big.Int).Lsh(z2, uint(m2)*2)
+	unit2 := new(big.Int).Sub(z1, z2)
+	unit2 = new(big.Int).Sub(unit2, z0)
+	unit2 = unit2.Lsh(unit2, uint(m2))
+	return unit1.Add(unit1, unit2).Add(unit1, z0)
 }
 
 func main() {
-	j := "123456789"
-	k := "987654321"
+	j := "3141592653589793238462643383279502884197169399375105820974944592"
+	k := "2718281828459045235360287471352662497757247093699959574966967627"
 	x := new(big.Int)
 	y := new(big.Int)
 	x.SetString(j, 10)
 	y.SetString(k, 10)
-	_, err := karatsuba(x, y)
-	if err != nil {
-		panic(fmt.Sprintf("Something went wrong: %s", err))
-	}
+	fmt.Printf("%d", karatsuba(x, y))
 }

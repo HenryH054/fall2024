2d8cf37c277a16694f4750a18038bf431eeb2c3d
Henry <144753375+HenryH054@users.noreply.github.com>
2024-10-02 16:42:18 -0600

    Update 5.7.py, switched metrics for measuring

--- 5.7.py
+++ 5.7.py
@@ -28,28 +28,48 @@ def quicksortpivotrand(arr):
     right = [x for x in arr if x > pivot]
     return quicksortpivotrand(left) + middle + quicksortpivotrand(right)
 
-for y in range(100):
+def quicksortpivotrandofthree(arr):
+    if len(arr) <= 1:
+        return arr
+    random_var = (random.randint(0, len(arr) - 1) + random.randint(0, len(arr) - 1) + random.randint(0, len(arr) - 1)) // 3
+    pivot = arr[random_var]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksortpivotrandofthree(left) + middle + quicksortpivotrandofthree(right)
+
+
+runtime = [0,0,0,0]
+for y in range(1000):
 
     rand_arr = []
-    n=100
+    n=random.randint(100,5000)
     for i in range(n):
         rand_arr.append(random.randint(1,9999))
 
     start_time = time.time()
-    for x in range(10000):
+    for x in range(10):
         arr= rand_arr
         quicksortpivotmid(arr)
-    print(f"Quick sort mid point pivot: {time.time() - start_time}")
+    runtime[0] += time.time() - start_time 
 
 
     start_time = time.time()
-    for x in range(10000):
+    for x in range(10):
         arr = rand_arr
         quicksortpivot0(arr)
-    print(f"Quick sort 0 pivot: {time.time() - start_time}")
+    runtime[1] += time.time() - start_time 
 
     start_time = time.time()
-    for x in range(10000):
+    for x in range(10):
         arr = rand_arr
         quicksortpivotrand(arr)
-    print(f"Quick sort random point pivot {time.time() - start_time}\n")
+    runtime[2] += time.time() - start_time 
+
+    start_time = time.time()
+    for x in range(10):
+        arr = rand_arr
+        quicksortpivotrandofthree(arr)
+    runtime[3] += time.time() - start_time 
+
+print(f"Midpoint: {runtime[0]/1000}\nZero: {runtime[1]/1000}\nRandom:{runtime[2]/1000}\n3Random: {runtime[3]/1000}")
